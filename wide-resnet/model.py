c0c3b1a10273e5a5578f4732e90b36b680b2842a
Yash <ysh.kant@gmail.com>
2018-12-18 22:05:12 +0530

    Debug'

--- wide-resnet/model.py
+++ wide-resnet/model.py
@@ -15,48 +15,6 @@ from keras import backend as K
 os.environ["CUDA_VISIBLE_DEVICES"]= sys.argv[1]
 tf.enable_eager_execution()
 
-# Fetch and format the mnist data
-# (mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data()
-
-# dataset = tf.data.Dataset.from_tensor_slices(
-#   (tf.cast(mnist_images[...,tf.newaxis]/255, tf.float32),
-#    tf.cast(mnist_labels,tf.int64)))
-# dataset = dataset.shuffle(1000).batch(32)
-
-# # Build the model
-# mnist_model = tf.keras.Sequential([
-#   tf.keras.layers.Conv2D(16,[3,3], activation='relu'),
-#   tf.keras.layers.Conv2D(16,[3,3], activation='relu'),
-#   tf.keras.layers.GlobalAveragePooling2D(),
-#   tf.keras.layers.Dense(10)
-# ])
-
-# for images,labels in dataset.take(1):
-#   print("Logits: ", mnist_model(images[0:1]).numpy())
-
-
-# optimizer = tf.train.AdamOptimizer()
-
-# loss_history = []
-
-
-# for (batch, (images, labels)) in enumerate(dataset.take(400)):
-#   if batch % 80 == 0:
-#     print()
-#   print('.', end='')
-#   with tf.GradientTape() as tape:
-#     logits = mnist_model(images, training=True)
-#     loss_value = tf.losses.sparse_softmax_cross_entropy(labels, logits)
-
-#   loss_history.append(loss_value.numpy())
-#   grads = tape.gradient(loss_value, mnist_model.variables)
-#   optimizer.apply_gradients(zip(grads, mnist_model.variables),
-#                             global_step=tf.train.get_or_create_global_step())
-
-
-
-
-
 
 batch_size = 100
 nb_epoch = 100
@@ -130,6 +88,7 @@ for e in range(epochs):
 #   grads = tape.gradient(loss_value, mnist_model.variables)
 #   optimizer.apply_gradients(zip(grads, mnist_model.variables),
 #                             global_step=tf.train.get_or_create_global_step())
+
 # model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["acc"])
 # print("Finished compiling")
 # print("Allocating GPU memory")
