011b1bd200be82c3d109ed00df8835a09a00758b
Yash <ysh.kant@gmail.com>
2018-12-19 00:12:52 +0530

    Building Wide-resnet

--- wide-resnet/wide_resnet.py
+++ wide-resnet/wide_resnet.py
@@ -21,13 +21,48 @@ from keras.layers.normalization import BatchNormalization
 from keras.utils.layer_utils import convert_all_kernels_in_model
 from keras.utils.data_utils import get_file
 from keras.engine.topology import get_source_inputs
-from keras_applications.imagenet_utils import _obtain_input_shape
+# from keras_applications.imagenet_utils import _obtain_input_shape
 import keras.backend as K
 
 TH_WEIGHTS_PATH = 'https://github.com/titu1994/Wide-Residual-Networks/releases/download/v1.2/wrn_28_8_th_kernels_th_dim_ordering.h5'
 TF_WEIGHTS_PATH = 'https://github.com/titu1994/Wide-Residual-Networks/releases/download/v1.2/wrn_28_8_tf_kernels_tf_dim_ordering.h5'
 TH_WEIGHTS_PATH_NO_TOP = 'https://github.com/titu1994/Wide-Residual-Networks/releases/download/v1.2/wrn_28_8_th_kernels_th_dim_ordering_no_top.h5'
 TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/titu1994/Wide-Residual-Networks/releases/download/v1.2/wrn_28_8_tf_kernels_tf_dim_ordering_no_top.h5'
+
+
+
+class WRNModel(tf.keras.Model):
+
+    def __init__(self, depth=28, width=8, dropout_rate=0.0,
+                        include_top=True, weights='cifar10',
+                        input_tensor=None, input_shape=None,
+                        classes=10, activation='softmax'):
+        super(WRNModel, self).__init__()
+
+        self.depth = depth
+        self.width = width
+        self.dropout_rate = dropout_rate
+        self.include_top = include_top
+        self.weights = weights
+        self.input_tensor = input_tensor
+        
+        if weights not in {'cifar10', None}:
+            raise ValueError('The `weights` argument should be either '
+                             '`None` (random initialization) or `cifar10` '
+                             '(pre-training on CIFAR-10).')
+
+        if weights == 'cifar10' and include_top and classes != 10:
+            raise ValueError('If using `weights` as CIFAR 10 with `include_top`'
+                             ' as true, `classes` should be 10')
+
+        if (depth - 4) % 6 != 0:
+            raise ValueError('Depth of the network must be such that (depth - 4)'
+                             'should be divisible by 6.')
+
+
+
+
+
 
 
 def WideResidualNetwork(depth=28, width=8, dropout_rate=0.0,
@@ -84,23 +119,25 @@ def WideResidualNetwork(depth=28, width=8, dropout_rate=0.0,
         raise ValueError('Depth of the network must be such that (depth - 4)'
                          'should be divisible by 6.')
 
-    # Determine proper input shape
-    input_shape = _obtain_input_shape(input_shape,
-                                      default_size=32,
-                                      min_size=8,
-                                      data_format=K.image_dim_ordering(),
-                                      require_flatten=include_top)
+    # # Determine proper input shape
+    # input_shape = _obtain_input_shape(input_shape,
+    #                                   default_size=32,
+    #                                   min_size=8,
+    #                                   data_format=K.image_dim_ordering(),
+    #                                   require_flatten=include_top)
 
-    if input_tensor is None:
-        img_input = Input(shape=input_shape)
-    else:
-        if not K.is_keras_tensor(input_tensor):
-            img_input = Input(tensor=input_tensor, shape=input_shape)
-        else:
-            img_input = input_tensor
+    # if input_tensor is None:
+    #     img_input = Input(shape=input_shape)
+    # else:
+    #     if not K.is_keras_tensor(input_tensor):
+    #         img_input = Input(tensor=input_tensor, shape=input_shape)
+    #     else:
+    #         img_input = input_tensor
 
-    x = __create_wide_residual_network(classes, img_input, include_top, depth, width,
+    x = __create_wide_residual_network(classes, include_top, depth, width,
                                        dropout_rate, activation)
+
+    print(x)
 
     # Ensure that the model takes into account
     # any potential predecessors of `input_tensor`.
@@ -108,153 +145,113 @@ def WideResidualNetwork(depth=28, width=8, dropout_rate=0.0,
         inputs = get_source_inputs(input_tensor)
     else:
         inputs = img_input
+    
     # Create model.
-    model = Model(inputs, x, name='wide-resnet')
+    # model = Model(inputs, x, name='wide-resnet')
 
     # load weights
-    if weights == 'cifar10':
-        if (depth == 28) and (width == 8) and (dropout_rate == 0.0):
-            # Default parameters match. Weights for this model exist:
-
-            if K.image_dim_ordering() == 'th':
-                if include_top:
-                    weights_path = get_file('wide_resnet_28_8_th_dim_ordering_th_kernels.h5',
-                                            TH_WEIGHTS_PATH,
-                                            cache_subdir='models')
-                else:
-                    weights_path = get_file('wide_resnet_28_8_th_dim_ordering_th_kernels_no_top.h5',
-                                            TH_WEIGHTS_PATH_NO_TOP,
-                                            cache_subdir='models')
-
-                model.load_weights(weights_path)
-
-                if K.backend() == 'tensorflow':
-                    warnings.warn('You are using the TensorFlow backend, yet you '
-                                  'are using the Theano '
-                                  'image dimension ordering convention '
-                                  '(`image_dim_ordering="th"`). '
-                                  'For best performance, set '
-                                  '`image_dim_ordering="tf"` in '
-                                  'your Keras config '
-                                  'at ~/.keras/keras.json.')
-                    convert_all_kernels_in_model(model)
-            else:
-                if include_top:
-                    weights_path = get_file('wide_resnet_28_8_tf_dim_ordering_tf_kernels.h5',
-                                            TF_WEIGHTS_PATH,
-                                            cache_subdir='models')
-                else:
-                    weights_path = get_file('wide_resnet_28_8_tf_dim_ordering_tf_kernels_no_top.h5',
-                                            TF_WEIGHTS_PATH_NO_TOP,
-                                            cache_subdir='models')
-
-                model.load_weights(weights_path)
-
-                if K.backend() == 'theano':
-                    convert_all_kernels_in_model(model)
 
     return model
 
 
-def __conv1_block(input):
-    x = Conv2D(16, (3, 3), padding='same')(input)
+def __conv1_block():
 
+    model = []
+    out_channels = 16
+    model.append(Conv2D(out_channels, (3, 3), padding='same'))
+    # x = Conv2D(16, (3, 3), padding='same')(input)
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    model.append(BatchNormalization(axis=channel_axis))
+    model.append(Activation('relu'))
+    # x = BatchNormalization(axis=channel_axis)(x)
+    # x = Activation('relu')(x) 
+    return out_channels, model
 
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
-    return x
 
+def __conv2_block(input_channels, k=1, dropout=0.0):
 
-def __conv2_block(input, k=1, dropout=0.0):
-    init = input
-
+    model_x = []
+    model_y = []
+    out_channels = 16*k
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
     # Check if input number of filters is same as 16 * k, else create convolution2d for this input
-    if K.image_data_format() == 'channels_first':
-        if init._keras_shape[1] != 16 * k:
-            init = Conv2D(16 * k, (1, 1), activation='linear', padding='same')(init)
-    else:
-        if init._keras_shape[-1] != 16 * k:
-            init = Conv2D(16 * k, (1, 1), activation='linear', padding='same')(init)
+    if input_channels != 16 * k:
+        model_x.append(Conv2D(16 * k, (1, 1), activation='linear', padding='same'))
 
-    x = Conv2D(16 * k, (3, 3), padding='same')(input)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(16 * k, (3, 3), padding='same')) 
+    model_y.append(BatchNormalization(axis=channel_axis)) 
+    model_y.append( Activation('relu'))
 
     if dropout > 0.0:
-        x = Dropout(dropout)(x)
+        model_y.append(Dropout(dropout)(x)) 
 
-    x = Conv2D(16 * k, (3, 3), padding='same')(x)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(16 * k, (3, 3), padding='same'))
+    model_y.append(BatchNormalization(axis=channel_axis))
+    model_y.append(Activation('relu'))
 
-    m = add([init, x])
-    return m
+    # m = add([init, x])
+    return out_channels, [model_x, model_y]
 
 
-def __conv3_block(input, k=1, dropout=0.0):
-    init = input
+def __conv3_block(input_channels, k=1, dropout=0.0):
+
+    model_x = []
+    model_y = []
+    out_channels = 32*k
 
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
-    # Check if input number of filters is same as 32 * k, else create convolution2d for this input
-    if K.image_data_format() == 'channels_first':
-        if init._keras_shape[1] != 32 * k:
-            init = Conv2D(32 * k, (1, 1), activation='linear', padding='same')(init)
-    else:
-        if init._keras_shape[-1] != 32 * k:
-            init = Conv2D(32 * k, (1, 1), activation='linear', padding='same')(init)
+    if input_channels != 32 * k:
+        model_x.append(Conv2D(32 * k, (1, 1), activation='linear', padding='same'))
 
-    x = Conv2D(32 * k, (3, 3), padding='same')(input)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(32 * k, (3, 3), padding='same'))
+    model_y.append(BatchNormalization(axis=channel_axis))
+    model_y.append(Activation('relu'))
 
     if dropout > 0.0:
-        x = Dropout(dropout)(x)
+        model_y.append(Dropout(dropout))
 
-    x = Conv2D(32 * k, (3, 3), padding='same')(x)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(32 * k, (3, 3), padding='same'))
+    model_y.append(BatchNormalization(axis=channel_axis))
+    model_y.append(Activation('relu'))
 
-    m = add([init, x])
-    return m
+    # m = add([init, x])
+    return out_channels, [model_x, model_y]
 
 
-def ___conv4_block(input, k=1, dropout=0.0):
-    init = input
+def ___conv4_block(input_channels, k=1, dropout=0.0):
+
+    model_x = []
+    model_y = []
+    out_channels = 64*k
 
     channel_axis = 1 if K.image_dim_ordering() == 'th' else -1
 
-    # Check if input number of filters is same as 64 * k, else create convolution2d for this input
-    if K.image_dim_ordering() == 'th':
-        if init._keras_shape[1] != 64 * k:
-            init = Conv2D(64 * k, (1, 1), activation='linear', padding='same')(init)
-    else:
-        if init._keras_shape[-1] != 64 * k:
-            init = Conv2D(64 * k, (1, 1), activation='linear', padding='same')(init)
+    if input_channels != 64 * k:
+        model_x.append(Conv2D(64 * k, (1, 1), activation='linear', padding='same'))
 
-    x = Conv2D(64 * k, (3, 3), padding='same')(input)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(64 * k, (3, 3), padding='same'))
+    model_y.append(BatchNormalization(axis=channel_axis))
+    model_y.append(Activation('relu'))
 
     if dropout > 0.0:
-        x = Dropout(dropout)(x)
+        model_y.append(Dropout(dropout))
 
-    x = Conv2D(64 * k, (3, 3), padding='same')(x)
-    x = BatchNormalization(axis=channel_axis)(x)
-    x = Activation('relu')(x)
+    model_y.append(Conv2D(64 * k, (3, 3), padding='same'))
+    model_y.append(BatchNormalization(axis=channel_axis))
+    model_y.append(Activation('relu'))
 
-    m = add([init, x])
-    return m
+
+    # m = add([init, x])
+    return out_channels, [model_x, model_y]
 
 
 
 # Enclose the function below in the call function of the model! 
 
 
-def __create_wide_residual_network(nb_classes, img_input, include_top, depth=28,
+def __create_wide_residual_network(nb_classes, include_top, depth=28,
                                    width=8, dropout=0.0, activation='softmax'):
     ''' Creates a Wide Residual Network with specified parameters
 
@@ -274,27 +271,38 @@ def __create_wide_residual_network(nb_classes, img_input, include_top, depth=28,
 
     N = (depth - 4) // 6
 
-    x = __conv1_block(img_input)
+    model = []
+    channels, blk = __conv1_block()
+    model.append(blk)
     nb_conv = 4
 
+
     for i in range(N):
-        x = __conv2_block(x, width, dropout)
+        channels, blk = __conv2_block(channels, width, dropout)
+        model.append(blk)
         nb_conv += 2
 
-    x = MaxPooling2D((2, 2))(x)
+    model.append([MaxPooling2D((2, 2))])
+
+    # x = MaxPooling2D((2, 2))(x)
 
     for i in range(N):
-        x = __conv3_block(x, width, dropout)
+        channels, blk = __conv3_block(channels, width, dropout)
+        model.append(blk)
         nb_conv += 2
 
-    x = MaxPooling2D((2, 2))(x)
+    model.append([MaxPooling2D((2, 2))])
+    # x = MaxPooling2D((2, 2))(x)
+
 
     for i in range(N):
-        x = ___conv4_block(x, width, dropout)
+        channels, blk = ___conv4_block(channels, width, dropout)
+        model.append(blk)
         nb_conv += 2
 
     if include_top:
-        x = GlobalAveragePooling2D()(x)
-        x = Dense(nb_classes, activation=activation)(x)
+        model.append([GlobalAveragePooling2D(), Dense(nb_classes, activation=activation)])
+        # x = GlobalAveragePooling2D()(x)
+        # x = Dense(nb_classes, activation=activation)(x)
 
-    return x
+    return model
