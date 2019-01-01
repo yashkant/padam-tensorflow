c24b6c7ad0e7e31409e7f90863f0f916750f4101
Harshal Mittal <harshalmittal4@gmail.com>
2019-01-01 13:22:50 +0530

    changes

--- run.py
+++ run.py
@@ -139,7 +139,7 @@ for optimizer in ['padam', 'adam', 'adamw', 'amsgrad', 'sgd']:
     dummy_x = tf.zeros((1, 32, 32, 3))
     #model._set_inputs(dummy_x)
     #print(model(dummy_x).shape)    
-    #history_resnet[optimizer] = model.fit(trainX, trainY, batch_size=batch_size, epochs=epochs, validation_data=(testX, testY), verbose=1)
+    history_resnet[optimizer] = model.fit(trainX, trainY, batch_size=batch_size, epochs=epochs, validation_data=(testX, testY), verbose=1)
 
 
 #train plot
