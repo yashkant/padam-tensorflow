38afb1a364b1b150531649f777c48550b20a8dbf
Yash Kant <ysh.kant@gmail.com>
2019-01-09 13:31:54 +0530

    Update run.py

--- resnet-18/run.py
+++ resnet-18/run.py
@@ -153,42 +153,6 @@ tf.train.create_global_step()
 datagen_train = ImageDataGenerator(preprocessing_function=preprocess,horizontal_flip=True)
 datagen_test = ImageDataGenerator(preprocessing_function=normalize)
 
-<<<<<<< HEAD
-optim_array = ['padam']
-p_values = [0.25, 0.125, 0.0625]
-
-history = {}
-
-for i in range(1, 3):
-    if(i != 0):
-        continue_training = True # Flag to continue training   
-        continue_epoch = (i)*30
-    else:
-        continue_training = False
-    
-    for optimizer in optim_array:
-        for p_value in p_values: 
-            print('-'*40, optimizer, '-'*40)
-            op = optim_params[optimizer]
-            
-            op['lr'] = op['lr']/(10**i)
-
-
-            if optimizer == 'adamw' and dataset=='imagenet':
-                op['weight_decay'] = 0.05 
-
-            if optimizer is not 'adamw':
-                model = Resnet(data_format='channels_last', classes=hp['classes'], wt_decay = op['weight_decay'])
-            else:
-                model = Resnet(data_format='channels_last', classes=hp['classes'], wt_decay  = 0)
-
-            model._set_inputs(tf.zeros((batch_size, 32, 32, 3)))
-
-            learning_rate = tf.train.exponential_decay(op['lr'], tf.train.get_global_step() * batch_size,
-                                               hp['decay_after']*train_size, 0.1, staircase=True)
-
-            logfile = 'log_'+str(p_value)+ '_' + dataset +'.csv'
-=======
 optim_array = ['amsgrad', 'sgd', 'adam', 'padam']
 
 
@@ -203,13 +167,9 @@ for i in range(2):
         
     for optimizer in optim_array:
 
-        # logfile = 'log_'+optimizer+ '_' + dataset +'.csv'
         print('-'*40, optimizer, '-'*40)
-
-
         op = optim_params[optimizer]
         op['lr'] = op['lr']/(10**i)
-        
 
         if optimizer == 'adamw' and dataset=='imagenet':
             op['weight_decay'] = 0.05 
@@ -232,17 +192,7 @@ for i in range(2):
             model = load_model(load_model_filepath, model)
         else:
             save_model_filepath = 'model_'+optimizer+'_'  + dataset + '_epochs'+ str(epochs)+'.h5'
->>>>>>> ed837c8c9448174df995ac1f05d6c0a73b42c576
 
-            if(continue_training):
-                load_model_filepath = 'model_'+str(p_value)+'_'  + dataset + '_epochs'+ str(continue_epoch)+'.h5'
-                save_model_filepath = 'model_'+str(p_value)+'_'  + dataset + '_epochs'+ str(continue_epoch+epochs)+'.h5'
-                model = load_model(load_model_filepath, model)
-            else:
-                save_model_filepath = 'model_'+str(p_value)+'_'  + dataset + '_epochs'+ str(epochs)+'.h5'
-
-<<<<<<< HEAD
-=======
         if optimizer == 'padam':
             optim = Padam(learning_rate=learning_rate, p=op['p'], beta1=op['b1'], beta2=op['b2'])
         elif optimizer == 'adam':
@@ -254,26 +204,7 @@ for i in range(2):
             optim = AMSGrad(learning_rate=learning_rate, beta1=op['b1'], beta2=op['b2'])
         elif optimizer == 'sgd':
             optim = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=op['m'])
->>>>>>> ed837c8c9448174df995ac1f05d6c0a73b42c576
 
-            if optimizer == 'padam':
-                optim = Padam(learning_rate=learning_rate, p= p_value, beta1=op['b1'], beta2=op['b2'])
-
-<<<<<<< HEAD
-            model.compile(optimizer=optim, loss='categorical_crossentropy', metrics=['accuracy', 'top_k_categorical_accuracy'], global_step=tf.train.get_global_step())
-
-            csv_logger = CSVLogger(logfile, append=True, separator=';')
-
-            history[optimizer] = model.fit_generator(datagen_train.flow(trainX, trainY, batch_size = batch_size), epochs = epochs, 
-                                         validation_data = datagen_test.flow(testX, testY, batch_size = batch_size), verbose=1, callbacks = [csv_logger])
-
-            scores = model.evaluate_generator(datagen_test.flow(testX, testY, batch_size = batch_size), verbose=1)
-
-            print("Final test loss and accuracy:", scores)
-            #filepath = 'model_'+optimizer+'_'  + dataset + '.h5'
-            save_model(save_model_filepath, model)
-            #f.close()
-=======
         model.compile(optimizer=optim, loss='categorical_crossentropy', metrics=['accuracy', 'top_k_categorical_accuracy'], global_step=tf.train.get_global_step())
 
         csv_logger = CSVLogger(logfile, append=True, separator=';')
@@ -284,10 +215,7 @@ for i in range(2):
         scores = model.evaluate_generator(datagen_test.flow(testX, testY, batch_size = batch_size), verbose=1)
 
         print("Final test loss and accuracy:", scores)
-        #filepath = 'model_'+optimizer+'_'  + dataset + '.h5'
         save_model(save_model_filepath, model)
-        #f.close()
->>>>>>> ed837c8c9448174df995ac1f05d6c0a73b42c576
 
 #train plot
 plt.figure(1)
